bd6680b7cd3e01549cc1e35ef5357a5e509dfac1
unknown <lfvanegas@gmail.com>
2019-02-11 11:52:59 -0500

    ejemplo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+
+// data
 import { todos } from './todos.json';
+
+// subcomponents
 import TodoForm from './components/TodoForm';
-console.log(todos)
 
 class App extends Component {
   constructor() {
@@ -11,51 +14,78 @@ class App extends Component {
     this.state = {
       todos
     }
+    this.handleAddTodo = this.handleAddTodo.bind(this);
   }
+
+  removeTodo(index) {
+    this.setState({
+      todos: this.state.todos.filter((e, i) => {
+        return i !== index
+      })
+    });
+  }
+
+  handleAddTodo(todo) {
+    this.setState({
+      todos: [...this.state.todos, todo]
+    })
+  }
+
   render() {
     const todos = this.state.todos.map((todo, i) => {
       return (
-        <div className="col-3 col-md-4">
-          <div className='card mt-4'>
-            <div className="card-header">
+        <div className="col-md-4" key={i}>
+          <div className="card mt-4">
+            <div className="card-title text-center">
               <h3>{todo.title}</h3>
-              <span className="badge-pill badge-warning ml-2">{todo.priority}</span>
+              <span className="badge badge-pill badge-danger ml-2">
+                {todo.priority}
+              </span>
             </div>
             <div className="card-body">
-              <p>{todo.description}</p>
-              <p><mark>{todo.responsible}</mark></p>
+              {todo.description}
+            </div>
+            <div className="card-footer">
+              <button
+                className="btn btn-danger"
+                onClick={this.removeTodo.bind(this, i)}>
+                Delete
+              </button>
             </div>
           </div>
         </div>
       )
-    }
-    )
-    return (
+    });
 
+    // RETURN THE COMPONENT
+    return (
       <div className="App">
-        <nav className="navbar navbar-dark bg-dark">        
-          <a className="text-white">Tareas
-          <span className="badge badge-pill badge-light ml-2">
+
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="navbar-brand" href="/">
+            Tasks
+            <span className="badge badge-pill badge-light ml-2">
               {this.state.todos.length}
             </span>
           </a>
         </nav>
-       
+
         <div className="container">
-          <div className="row">     
-            <div className= "col-md-4">     
-              <img src={logo} className="App-logo" alt="logo" />
-              <TodoForm />
+          <div className="row mt-4">
+
+            <div className="col-md-4 text-center">
+                <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm onAddTodo={this.handleAddTodo}></TodoForm>
             </div>
-          
+
             <div className="col-md-8">
               <div className="row">
                 {todos}
-              </div>             
-            </div>         
-        </div>      
+              </div>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
     );
   }
 }
