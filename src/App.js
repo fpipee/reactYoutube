5d51b1d656443ee223d7df99038f77f7574aa2f7
unknown <lfvanegas@gmail.com>
2019-02-11 00:08:39 -0500

    Segundo video de Fatz

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { todos } from './todos.json'
+console.log(todos)
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      todos
+    }
+  }
   render() {
+    const todos = this.state.todos.map((todo, i) => {
+      return (
+        <div className="col-3 col-md-4">
+          <div className='card mt-4'>
+            <div className="card-header">
+              <h3>{todo.title}</h3>
+              <span className="badge-pill badge-warning ml-2">{todo.priority}</span>
+            </div>
+            <div className="card-body">
+            <p>{todo.description}</p>
+            <p><mark>{todo.responsible}</mark></p>
+            </div>
+          </div>
+        </div>
+      )
+    }
+    )
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
+        <nav className="navbar navbar-dark bg-dark">
+          <a className="text-white">Tareas
+          <span className="badge badge-pill badge-light ml-2">
+            {this.state.todos.length}
+          </span>
           </a>
-        </header>
+        </nav>
+        <div className="container">
+          <div className="row mt-4">
+            {todos}
+          </div>
+        </div>
+
+        <img src={logo} className="App-logo" alt="logo" />
       </div>
     );
   }
