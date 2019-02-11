9f8642dbbcaa8757d35de46910ae1940afb3d497
unknown <lfvanegas@gmail.com>
2019-02-11 11:34:45 -0500

    normalizada para video 3

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { todos } from './todos.json'
+import { todos } from './todos.json';
+import TodoForm from './components/TodoForm';
 console.log(todos)
 
 class App extends Component {
@@ -21,8 +22,8 @@ class App extends Component {
               <span className="badge-pill badge-warning ml-2">{todo.priority}</span>
             </div>
             <div className="card-body">
-            <p>{todo.description}</p>
-            <p><mark>{todo.responsible}</mark></p>
+              <p>{todo.description}</p>
+              <p><mark>{todo.responsible}</mark></p>
             </div>
           </div>
         </div>
@@ -30,22 +31,31 @@ class App extends Component {
     }
     )
     return (
+
       <div className="App">
-        <nav className="navbar navbar-dark bg-dark">
+        <nav className="navbar navbar-dark bg-dark">        
           <a className="text-white">Tareas
           <span className="badge badge-pill badge-light ml-2">
-            {this.state.todos.length}
-          </span>
+              {this.state.todos.length}
+            </span>
           </a>
         </nav>
+       
         <div className="container">
-          <div className="row mt-4">
-            {todos}
-          </div>
-        </div>
-
-        <img src={logo} className="App-logo" alt="logo" />
+          <div className="row">     
+            <div className= "col-md-4">     
+              <img src={logo} className="App-logo" alt="logo" />
+              <TodoForm />
+            </div>
+          
+            <div className="col-md-8">
+              <div className="row">
+                {todos}
+              </div>             
+            </div>         
+        </div>      
       </div>
+    </div>
     );
   }
 }
--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -1,6 +1,20 @@
 import React,{Component} from 'react';
 
-class TodoForm extends  Component {
+class TodoForm extends Component {
+  constructor(){
+    super();
+    this.state = {
+      title:'',
+      responsible:'',
+      description:'',
+      priority:'low',
+    };
+  }
+
+  handleInputChange (){
+    console.log('writing');
+
+  }
     render(){
         return (
             <div className="card">
